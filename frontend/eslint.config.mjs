d3c65a69421807791b740440a8598df79eeddeab
Chamara Janith <chamaragithub21@gmail.com>
2026-07-15 16:48:22 +0530

    feat: initialize ESLint configuration with custom overrides for Next.js and TypeScript

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off"
+    }
+  }
 ]);
 
 export default eslintConfig;
